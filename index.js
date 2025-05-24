f39e9c8a921138d317ac1d972c9906973672be51
Pritesh <bhuravanepritesh@gmail.com>
2025-05-24 08:31:03 +0530

    Index.js Added

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+  console.log('Serving file from:', filePath);
+  res.sendFile(filePath);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
